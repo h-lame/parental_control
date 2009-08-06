be3d969afb70b9517b5504d9bb71076553240bab
Murray Steele <muz@h-lame.com>
2009-08-06 09:19:34 +0100

    Get around some autoloading issues when using activerecord on it's own.
    We need to refer to ActiveRecord::ActiveRecordError before we refer to ActiveRecord::Associations because of some autoload issues.  See https://rails.lighthouseapp.com/projects/8994/tickets/2577 for more details.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,8 @@ $: << File.join(File.dirname(__FILE__))
 require 'rubygems'
 require 'test/unit'
 require 'activerecord'
+# HACK make sure we refer to AR:ARE before init so that AR:Assocations is there
+# see: https://rails.lighthouseapp.com/projects/8994/tickets/2577
+ActiveRecord::ActiveRecordError 
 require 'init'
 require 'models'
